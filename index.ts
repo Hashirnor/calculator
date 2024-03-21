5762cf0a1db0946ebc8fc6da054f22182f34c62a
Hashirnor <hashirnoor925@gmail.com>
2024-03-21 22:27:41 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const result = await inquirer.prompt([
+{message: "enter first number", type: "number", name:"firstNumber"},
+{message:"enter second number", type:"number", name:"secondNumber"},
+{message:"select one of the operator to perform your operation",
+type:"list",
+name:"operator",
+choices:["addition","subtraction","multiply","division"],
+},
+]);
+// condition
+if(result.operator ==="addition"){
+    console.log(result.firstNumber + result.secondNumber);
+}
+else if(result.operator === "subtraction"){
+    console.log(result.firstNumber - result.secondNumber);
+}
+else if(result.operator ==="multiply"){
+    console.log(result.firstNumber * result.secondNumber);
+}
+else if(result.operator ==="division"){
+    console.log(result.firstNumber / result.secondNumber);
+}
+else{
+    console.log("please select valid operator");
+}
